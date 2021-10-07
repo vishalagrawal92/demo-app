d209994c190d93bb1e7140d9ca5f0aa160463341
kcchaitanya <connecttokc@gmail.com>
2021-10-07 10:17:48 +0530

    Updated text

--- src/main/java/com/example/demoapp/HelloController.java
+++ src/main/java/com/example/demoapp/HelloController.java
@@ -13,7 +13,7 @@ public class HelloController {
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello World 2!";
+        return "Hello World kc!";
     }
 
     public static void main(String[] args) {
